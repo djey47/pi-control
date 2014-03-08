39b0aa3cf71a877325baa4968c201c7b500292a1
djey <j.canler@gmail.com>
2014-03-08 21:57:27 +0100

    Added primary file structure

---
+++ web-services/rupees/pi_control.rb
@@ -0,0 +1,3 @@
+class PiControl
+  # To change this template use File | Settings | File Templates.
+end
---
+++ web-services/rupees/services.rb
@@ -0,0 +1,3 @@
+class Services
+  # To change this template use File | Settings | File Templates.
+end
