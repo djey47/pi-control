ec2ad1f0743210845876b2d7f3976c446a5b1925
djey <j.canler@gmail.com>
2014-03-08 22:13:42 +0100

    Added heartbeat web service

--- web-services/rupees/pi_control.rb
+++ web-services/rupees/pi_control.rb
@@ -1,3 +1,32 @@
+# pi_control.rb - app entry point
+
+require 'singleton'
+require 'logger'
+require_relative 'services'
+
 class PiControl
-  # To change this template use File | Settings | File Templates.
+  include Singleton
+
+  def initialize
+    @logger = Logger.new(STDOUT)
+    @logger.level = Logger::INFO
+  end
+
+  def run
+    server_thread = Thread.new {
+      @logger.info('[PiControl] Starting HTTP server...')
+      Services.run!
+    }
+    @logger.info('[PiControl] Ready to rumble!')
+    # Waiting for server thread to terminate
+    server_thread.join
+  end
+
+  def stop
+    @logger.info('[PiControl] Exiting pi-control.')
+  end
 end
+
+# Boot
+PiControl.instance.run
+PiControl.instance.stop
--- web-services/rupees/services.rb
+++ web-services/rupees/services.rb
@@ -1,3 +1,24 @@
-class Services
-  # To change this template use File | Settings | File Templates.
+# services.rb - REST http server
+
+require 'sinatra/base'
+require 'json'
+require 'logger'
+
+class Services < Sinatra::Base
+  def initialize
+    @logger = Logger.new(STDOUT)
+    @logger.level = Logger::INFO
+    super #Required for correct Sinatra init
+  end
+
+  #config
+  set :port, 4600
+  set :show_exceptions, true
+  set :environment, :development
+
+  #Heartbeat
+  get '/' do
+    @logger.info('[Services] Heartbeat!')
+    [200, 'pi-control - webservices are alive :)']
+  end
 end
