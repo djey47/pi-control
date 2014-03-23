8e7be88f40c44b7d3bed004e4c711fbc6dbcd7a4
djey <j.canler@gmail.com>
2014-03-23 19:24:33 +0100

    Added Virtual Machine class to object model

---
+++ web-services/rupees/model/virtual_machine.rb
@@ -0,0 +1,18 @@
+# virtual_machine.rb - represents a ESXi managed virtual machine.
+
+class VirtualMachine
+
+  attr_reader :id
+  attr_reader :name
+  attr_reader :guest_os
+
+  def initialize(id, name, guest_os)
+    @id = id
+    @name = name
+    @guest_os = guest_os
+  end
+
+  def to_json(*a)
+    { :id => @id, :name => @name, :guest_os => @guest_os }.to_json(*a)
+  end
+end
