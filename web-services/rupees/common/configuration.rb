adaf39c30f39c25b9fbd0f20ff17f709c55a60a3
jcanler <jerome.canler@ext.pmu.fr>
2014-07-11 14:36:53 +0200

    Done server port configuration

--- web-services/rupees/common/configuration.rb
+++ web-services/rupees/common/configuration.rb
@@ -7,8 +7,9 @@ require 'yaml'
 module Configuration
 
   CONFIG_FILE_NAME = './web-services/conf/pi-control.yml'
+  DEFAULT_SERVER_PORT = 4600
 
-  class Conf < Struct.new(:app_is_production, :esxi_host_name, :esxi_user, :esxi_mac_address, :lan_broadcast_address)
+  class Conf < Struct.new(:app_is_production, :app_server_port, :esxi_host_name, :esxi_user, :esxi_mac_address, :lan_broadcast_address)
   end
 
   @logger = Logger.new(STDOUT)
@@ -23,6 +24,10 @@ module Configuration
       contents = YAML.load_file(config_file_name)
 
       app_is_production = contents['app']['is-production']
+      
+      server_port = DEFAULT_SERVER_PORT
+      server_port = contents['app']['server-port'] if contents['app']['server-port']
+      
       host_name = contents['esxi']['host-name']
       user = contents['esxi']['user']
       mac_address = contents['esxi']['mac-address']
@@ -33,6 +38,6 @@ module Configuration
       raise('Invalid configuration')
     end
 
-    Conf.new(app_is_production, host_name, user, mac_address, broadcast_address)
+    Conf.new(app_is_production, server_port, host_name, user, mac_address, broadcast_address)
   end
 end
--- web-services/tests/common/configuration_test.rb
+++ web-services/tests/common/configuration_test.rb
@@ -7,17 +7,26 @@ require_relative '../../rupees/common/configuration'
 class ConfigurationTest < Test::Unit::TestCase
 
   CONFIG_FILE_NAME = './web-services/tests/conf/pi-control.yml'
+  CONFIG_FILE_NAME_NO_SERVER_PORT = './web-services/tests/conf/pi-control_no-server-port.yml'
   INVALID_CONFIG_FILE_NAME = './web-services/tests/conf/pi-control_invalid.yml'
 
   def test_get_should_return_conf_from_yaml
 
     config = Configuration::get(CONFIG_FILE_NAME)
 
-    assert_equal(false, config.app_is_production, 'Invalid config value: app_is_production')
-    assert_equal('test', config.esxi_host_name, 'Invalid config value: esxi_host_name')
-    assert_equal('tester', config.esxi_user, 'Invalid config value: esxi_user')
-    assert_equal('FF:FF:FF:FF:FF:FF', config.esxi_mac_address, 'Invalid config value: esxi_mac_address')
-    assert_equal('192.168.1.255', config.lan_broadcast_address, 'Invalid config value: lan_broadcast_address')
+    assert_equal(false, config.app_is_production, 'Invalid config value: app/is-production')
+    assert_equal(4646, config.app_server_port, 'Invalid config value: app/server-port')
+    assert_equal('test', config.esxi_host_name, 'Invalid config value: esxi/host-name')
+    assert_equal('tester', config.esxi_user, 'Invalid config value: esxi/user')
+    assert_equal('FF:FF:FF:FF:FF:FF', config.esxi_mac_address, 'Invalid config value: esxi/mac-address')
+    assert_equal('192.168.1.255', config.lan_broadcast_address, 'Invalid config value: lan/broadcast-address')
+  end
+
+  def test_get_server_port_without_setting_should_return_default_value
+
+    config = Configuration::get(CONFIG_FILE_NAME_NO_SERVER_PORT)
+
+    assert_equal(4600, config.app_server_port, 'Invalid config value: app/server-port')
   end
 
   def test_get_invalid_config_file_then_exception
