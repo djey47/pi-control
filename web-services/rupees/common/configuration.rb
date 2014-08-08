250faac7e2ec79f0aeb578b90fee0fbf85631507
djey <j.canler@gmail.com>
2014-08-09 00:36:20 +0200

    [9] Added cache directory as optional configuration setting

--- web-services/rupees/common/configuration.rb
+++ web-services/rupees/common/configuration.rb
@@ -7,9 +7,11 @@ require 'yaml'
 module Configuration
 
   CONFIG_FILE_NAME = './web-services/conf/pi-control.yml'
-  DEFAULT_SERVER_PORT = 4600
 
-  class Conf < Struct.new(:app_is_production, :app_server_port, :esxi_host_name, :esxi_user, :esxi_mac_address, :lan_broadcast_address)
+  DEFAULT_SERVER_PORT = 4600
+  DEFAULT_CACHE_DIRECTORY = '/tmp/pi-control-cache'
+
+  class Conf < Struct.new(:app_is_production, :app_server_port, :app_cache_directory, :esxi_host_name, :esxi_user, :esxi_mac_address, :lan_broadcast_address)
   end
 
   @logger = Logger.new(STDOUT)
@@ -27,6 +29,9 @@ module Configuration
       
       server_port = DEFAULT_SERVER_PORT
       server_port = contents['app']['server-port'] if contents['app']['server-port']
+
+      cache_directory = DEFAULT_CACHE_DIRECTORY
+      cache_directory = contents['app']['cache-directory'] if contents['app']['cache-directory']
       
       host_name = contents['esxi']['host-name']
       user = contents['esxi']['user']
@@ -38,6 +43,6 @@ module Configuration
       raise('Invalid configuration')
     end
 
-    Conf.new(app_is_production, server_port, host_name, user, mac_address, broadcast_address)
+    Conf.new(app_is_production, server_port, cache_directory, host_name, user, mac_address, broadcast_address)
   end
 end
--- web-services/tests/common/configuration_test.rb
+++ web-services/tests/common/configuration_test.rb
@@ -7,7 +7,7 @@ require_relative '../../rupees/common/configuration'
 class ConfigurationTest < Test::Unit::TestCase
 
   CONFIG_FILE_NAME = './web-services/tests/conf/pi-control.yml'
-  CONFIG_FILE_NAME_NO_SERVER_PORT = './web-services/tests/conf/pi-control_no-server-port.yml'
+  CONFIG_FILE_NAME_OPTIONAL = './web-services/tests/conf/pi-control_optional.yml'
   INVALID_CONFIG_FILE_NAME = './web-services/tests/conf/pi-control_invalid.yml'
 
   def test_get_should_return_conf_from_yaml
@@ -22,11 +22,12 @@ class ConfigurationTest < Test::Unit::TestCase
     assert_equal('192.168.1.255', config.lan_broadcast_address, 'Invalid config value: lan/broadcast-address')
   end
 
-  def test_get_server_port_without_setting_should_return_default_value
+  def test_get_optional_settings_without_values_should_return_default_values
 
-    config = Configuration::get(CONFIG_FILE_NAME_NO_SERVER_PORT)
+    config = Configuration::get(CONFIG_FILE_NAME_OPTIONAL)
 
     assert_equal(4600, config.app_server_port, 'Invalid config value: app/server-port')
+    assert_equal('/tmp/pi-control-cache', config.app_cache_directory, 'Invalid config value: app/cache-directory')
   end
 
   def test_get_invalid_config_file_then_exception
