701d90a13fba8cd7063d618da6ee92babc2044c3
jcanler <jerome.canler@ext.pmu.fr>
2014-08-13 18:50:27 +0200

    [9] Updated cache cleaner

--- web-services/tests/utils/caching_helper.rb
+++ web-services/tests/utils/caching_helper.rb
@@ -4,6 +4,8 @@ module CachingHelper
 
   def self.clear_caches
     File::delete("#{Configuration::get.app_cache_directory}/#{Services::CACHE_KEY_DISKS}.cache") rescue nil
+    File::delete("#{Configuration::get.app_cache_directory}/#{Services::CACHE_KEY_SMART_PREFIX}1.cache") rescue nil
     File::delete("#{Configuration::get.app_cache_directory}/#{Services::CACHE_KEY_SMART_PREFIX}2.cache") rescue nil
+    File::delete("#{Configuration::get.app_cache_directory}/#{Services::CACHE_KEY_SMART_PREFIX}1-2.cache") rescue nil
   end
 end
