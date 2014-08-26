d2ad4ab86a0668d57aa70a01d094e84b834fa39f
Jerome CANLER <jerome.canler@ext.pmu.fr>
2014-08-26 13:52:21 +0200

    [] Enhanced script to display tests results

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 OUT_FILE=./tests_results.out
 echo Testing pi-control web services...
-find ./web-services/tests/ -name *_test.rb -exec echo ============================== \; -exec ruby {} \; > $OUT_FILE
-echo Done! See results: less $OUT_FILE
+find ./web-services/tests/ -name *_test.rb -exec echo ============================== \; -exec ruby {} \; | tee $OUT_FILE
+echo ==============================
+echo Done! Results written to $OUT_FILE
