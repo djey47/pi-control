d22ea2dd78c26c0738f7b4cb96d14cc7c4a8e56d
djey <j.canler@gmail.com>
2014-10-23 01:50:54 +0200

    [2] Modify start script to perform on-demand check

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,37 @@
 #! /bin/sh
-echo Starting pi-control web services...
+if [  "$#" -gt 1 ]; then
+	echo "Usage: $0 [--check]"
+	echo "check: perform install check before starting pi-control."
+	exit 1
+fi
+
+function whichBinary {
+	which $1
+	if [  "$?" -ne "0" ]; then
+		echo $2
+		exit 1
+	fi	
+}
+
+if [  "$1" == "--check" ]; then
+	echo "* Performing install check..."
+	
+	echo "  > Ruby interpreter..."
+	whichBinary ruby "! ERROR: ruby script engine not installed, please see http://rvm.io"
+	whichBinary gem "! ERROR: ruby gem utility not installed, please see http://rvm.io"
+	gem list | grep "bundler"
+	if [  "$?" -ne "0" ]; then
+		echo "! ERROR: bundler gem not installed, please run 'gem install bundler && bundle install'"
+		exit 1
+	fi
+	
+	echo "  > Local commands..."
+	whichBinary ssh "! ERROR: ssh not installed, please run 'apt-get install ssh'"
+	whichBinary wakeonlan "! ERROR: wakeonlan not installed, please run 'apt-get install wakeonlan'"
+
+	echo "* Well Done!"
+fi
+
+echo "* Starting pi-control web services..."
 ruby ./web-services/rupees/pi_control.rb
+exit 0
