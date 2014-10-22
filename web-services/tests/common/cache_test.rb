826f91ce9ccb3dbacec2deef20f3f976abc7552e
Jerome CANLER <jerome.canler@ext.pmu.fr>
2014-10-22 11:07:04 +0200

    [16] Rename cache attributes + fix test

--- web-services/tests/common/cache_test.rb
+++ web-services/tests/common/cache_test.rb
@@ -5,19 +5,24 @@ require_relative '../../rupees/common/cache.rb'
 
 class CacheTest < Test::Unit::TestCase 
 
+	def setup
+    	Cache.instance.clear_all
+  	end
+
 	def test_initialize_should_instantiate_caches
-		assert_false(Cache.instance.disks_cache.nil?)
-		assert_false(Cache.instance.smart_cache.nil?)
+		assert_false(Cache.instance.disks.nil?)
+		assert_false(Cache.instance.smart.nil?)
 	end
 
 	def test_clear_all_should_remove_all_files
 		#given
 		cache_path = "#{Configuration::get.app_cache_directory}/*.cache"
-	    Cache.instance.disks_cache.cache(Cache::CACHE_KEY_DISKS) do
+		
+	    Cache.instance.disks.cache(Cache::CACHE_KEY_DISKS) do
 	      []
 	    end
 	    for i in 1..10 do
-		    Cache.instance.smart_cache.cache("#{Cache::CACHE_KEY_SMART_PREFIX}#{i}") do
+		    Cache.instance.smart.cache("#{Cache::CACHE_KEY_SMART_PREFIX}#{i}") do
 		      []
 		    end
 		end
